cd47004dde0150d9f9d5371f883a729f45c1f47e
unknown <leiregurru8@gmail.com>
2020-11-06 13:29:50 +0100

    aumento pantalla

--- src/Pantallas/InicioSesion.java
+++ src/Pantallas/InicioSesion.java
@@ -6,10 +6,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
 
 public class InicioSesion extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -35,8 +42,32 @@ public class InicioSesion extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(169, 58, 146, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(264, 181, 115, 29);
+		contentPane.add(btnLogin);
+		
+		JButton btnRegistrarse = new JButton("Registrarse");
+		btnRegistrarse.setBounds(99, 181, 115, 29);
+		contentPane.add(btnRegistrarse);
+		
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setBounds(81, 58, 96, 26);
+		contentPane.add(lblUsuario);
+		
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a");
+		lblContrasea.setBounds(69, 118, 96, 23);
+		contentPane.add(lblContrasea);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(169, 116, 146, 26);
+		contentPane.add(passwordField);
 	}
-
 }
--- src/Pantallas/Registrarse.java
+++ src/Pantallas/Registrarse.java
@@ -6,10 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Registrarse extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -32,11 +42,58 @@ public class Registrarse extends JFrame {
 	 */
 	public Registrarse() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 506, 499);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(48, 37, 69, 20);
+		contentPane.add(lblNombre);
+		
+		JLabel lblApellido = new JLabel("Apellido");
+		lblApellido.setBounds(48, 73, 69, 20);
+		contentPane.add(lblApellido);
+		
+		JLabel lblCorreo = new JLabel("Correo");
+		lblCorreo.setBounds(48, 109, 69, 20);
+		contentPane.add(lblCorreo);
+		
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a");
+		lblContrasea.setBounds(48, 152, 97, 20);
+		contentPane.add(lblContrasea);
+		
+		textField = new JTextField();
+		textField.setBounds(151, 34, 146, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(151, 70, 146, 26);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(151, 109, 146, 26);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(151, 149, 146, 26);
+		contentPane.add(passwordField);
+		
+		JButton btnAtrs = new JButton("Atr\u00E1s");
+		btnAtrs.setBounds(48, 199, 115, 29);
+		contentPane.add(btnAtrs);
+		
+		JButton btnRedistrarse = new JButton("Registrarse");
+		btnRedistrarse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnRedistrarse.setBounds(225, 199, 115, 29);
+		contentPane.add(btnRedistrarse);
 	}
 
 }
